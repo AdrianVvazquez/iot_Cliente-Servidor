263bc0989ed99f16f6229d12b5bd87af997b0cad
AdrianVvazquez <jose_a_vazquez@outlook.com>
2023-02-13 23:20:42 -0600

    Actividad de hilos .c

--- A2_Threads/A2Threads.c
+++ A2_Threads/A2Threads.c
@@ -1,14 +1,40 @@
+/* Create a finite number of threads to perform an addition action */
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
-#define MAX_CONN 5
+#define N_THREADS_PARAM 1
+/* Data shared by the threads */
+int32_t sum;
+/* EVery thread call this function */
+void *runner(void *param);
 
-INT32_T = sum;
-
-int main(int argc, char *arcgv[])
+int32_t main(int32_t argc, char *argv[])
 {
-    printf("argc: %d", argc);
-    return 0;
+	int32_t n_threads = atoi(argv[N_THREADS_PARAM]);
+	sum = 0;
+	/* Identifier */
+	pthread_t tid;
+	/* Attributes */
+	pthread_attr_t attr;
+	/* Set the default attributes of the thread*/
+	pthread_attr_init(&attr);
+	
+	for(int i=0; i<n_threads; i++){	
+		/* Create the thread */
+		pthread_create(&tid, &attr, runner, argv[N_THREADS_PARAM]);
+		/* Wait for the thread to exit */
+		pthread_join(tid, NULL);
+		printf("sum = %d\n", sum);
+	}
 }
+
+/* The thread will execute in this function */
+void *runner(void *param)
+{
+	sum += 5;
+	pthread_exit(0);
+}
+
